9e3bd4341b6f1be61043acbcb127b45fd70ad2e3
devanshroya-7 <jdevansh2000@gmail.com>
2026-06-27 22:37:47 +0000

    Add SignalBuilderModel.m

---
+++ src/App/components/forcing_builder/SignalBuilderModel.m
@@ -0,0 +1,22 @@
+classdef SignalBuilderModel < handle
+    % Model for signal builder. This handles creating and modifying
+    % different signals
+
+    properties
+        Property1
+    end
+
+    methods
+        function obj = untitled3(inputArg1,inputArg2)
+            %UNTITLED3 Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+    end
+end
