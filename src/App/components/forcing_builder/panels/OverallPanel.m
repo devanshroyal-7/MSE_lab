c7b51f657e24969f6c3376ed75a249587e401a35
devanshroyal-7 <jdevansh2000@gmail.com>
2026-07-13 00:47:40 +0000

    Add plot view switch

--- src/App/components/forcing_builder/panels/OverallPanel.m
+++ src/App/components/forcing_builder/panels/OverallPanel.m
@@ -8,13 +8,15 @@ classdef OverallPanel < handle
         ClearButton
         AddButton
         RemoveButton
+        ViewLabel
+        ViewSwitch
     end
 
     methods
         function obj = OverallPanel(parentContainer)
             obj.MainLayoutGrid = uigridlayout(parentContainer, [4, 3]);
-            obj.MainLayoutGrid.RowHeight = {30, 150, 30, 30};
-            obj.MainLayoutGrid.ColumnWidth = {150, 75, 75};
+            obj.MainLayoutGrid.RowHeight = {30, 130, 30, 30};
+            obj.MainLayoutGrid.ColumnWidth = {62, 62, 62, 62};
 
             obj.AvailableLabel = uilabel(obj.MainLayoutGrid, "Text", "Available", "FontWeight", "bold");
             
@@ -22,38 +24,48 @@ classdef OverallPanel < handle
 
             obj.AvailableListBox = uilistbox(obj.MainLayoutGrid, "Items", ["Custom", "Noise", "Ramp", "Sine", "Step", "Swept Sine", "Zero Output"]);
 
-            obj.OverallListBox = uilistbox(obj.MainLayoutGrid);
+            obj.OverallListBox = uilistbox(obj.MainLayoutGrid, "Items", "");
 
             obj.AddButton = uibutton(obj.MainLayoutGrid, "Text", "Add");
-            
+
             obj.RemoveButton = uibutton(obj.MainLayoutGrid, "Text", "Remove");
 
             obj.ClearButton = uibutton(obj.MainLayoutGrid, "Text", "Clear");
+
+            obj.ViewLabel = uilabel(obj.MainLayoutGrid, "Text", "Plot view: ", "VerticalAlignment", "bottom");
+
+            obj.ViewSwitch = uiswitch(obj.MainLayoutGrid, "Items", ["Single", "Overall"]);
 
             obj.layoutComponent();
         end
 
         function layoutComponent(obj)
             obj.AvailableLabel.Layout.Row = 1;
-            obj.AvailableLabel.Layout.Column = 1;
+            obj.AvailableLabel.Layout.Column = [1, 2];
 
             obj.OverallLabel.Layout.Row = 1;
-            obj.OverallLabel.Layout.Column = [2, 3];
+            obj.OverallLabel.Layout.Column = [3, 4];
 
             obj.AvailableListBox.Layout.Row = 2;
-            obj.AvailableListBox.Layout.Column = 1;
+            obj.AvailableListBox.Layout.Column = [1, 2];
 
             obj.OverallListBox.Layout.Row = 2;
-            obj.OverallListBox.Layout.Column = [2, 3];
+            obj.OverallListBox.Layout.Column = [3, 4];
 
             obj.AddButton.Layout.Row = 3;
-            obj.AddButton.Layout.Column = 2;
+            obj.AddButton.Layout.Column = 3;
 
             obj.RemoveButton.Layout.Row = 3;
-            obj.RemoveButton.Layout.Column = 3;
+            obj.RemoveButton.Layout.Column = 4;
 
             obj.ClearButton.Layout.Row = 4;
-            obj.ClearButton.Layout.Column = [2, 3];
+            obj.ClearButton.Layout.Column = [3, 4];
+
+            obj.ViewLabel.Layout.Row = 3;
+            obj.ViewLabel.Layout.Column = [1, 2];
+
+            obj.ViewSwitch.Layout.Row = 4;
+            obj.ViewSwitch.Layout.Column = [1, 2];
         end
     end
 end
