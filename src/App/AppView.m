c2cfd113280a05ae949b3f218c28889ef5af3956
devanshroyal-7 <jdevansh2000@gmail.com>
2026-08-04 16:53:10 +0000

    Replace simcontrol with a uibutton

--- src/App/AppView.m
+++ src/App/AppView.m
@@ -3,12 +3,15 @@ classdef AppView < handle
 
     properties 
         MainLayoutGrid
-        SimControl
+        SimStart
         TabGroup
         SaveButton
         % TimeTab
         TimeDomainPanel
         FreqDomainPanel
+
+        % Callback properties
+        fwdRunSimCallback
     end
 
     methods
@@ -17,7 +20,8 @@ classdef AppView < handle
             obj.MainLayoutGrid.Padding = [0, 10, 0, 10];
             obj.MainLayoutGrid.RowHeight = {60, '1x', '1x', '1x', '1x', '1x', '1x', '1x', '1x', 30};
             obj.MainLayoutGrid.ColumnWidth = {100, '1x', '1x', 100, 100, 100, '1x', '1x', 100, 100};
-
+            
+            % Title 
             titleGrid = uigridlayout(obj.MainLayoutGrid, [1, 1]);
             titleGrid.Layout.Column = [1, 6];
             titleGrid.Layout.Row = 1;
@@ -26,18 +30,22 @@ classdef AppView < handle
                 "FontWeight", "bold", ...
                 "FontSize", 25, ...
                 "HorizontalAlignment", 'left');
-            % CrsNumTitle.Layout.Column = [1, 6];
-            % CrsNumTitle.Layout.Row = 1;
 
-            obj.SimControl = uisimcontrols(obj.MainLayoutGrid, ...
-                'StartText', 'Start Test');
-            obj.SimControl.Layout.Column = [9, 10];
-            obj.SimControl.Layout.Row = 1;
+            % Simulation Controls
+            obj.SimStart = uibutton(obj.MainLayoutGrid, "state", ...
+                "Text", "Start Simulation", ...
+                "Icon","slrdt_start.png", ...
+                "IconAlignment","top", ...
+                "ButtonPushedFcn", @(~,~) obj.RunSimCallback);
+            obj.SimStart.Layout.Column = 9;
+            obj.SimStart.Layout.Row = 1;
 
+            % Tabs
             obj.TabGroup = uitabgroup(obj.MainLayoutGrid);
             obj.TabGroup.Layout.Column = [1, 10];
             obj.TabGroup.Layout.Row = [2, 9];
 
+            % Save Button
             SaveButtonGrid = uigridlayout(obj.MainLayoutGrid, [1, 2]);
             SaveButtonGrid.Layout.Column = [8, 10];
             SaveButtonGrid.Layout.Row = 10;
@@ -47,13 +55,22 @@ classdef AppView < handle
             obj.SaveButton.Layout.Column = 2;
             obj.SaveButton.Layout.Row = 1;
             
+            % Time Domain
             TimeTab = uitab(obj.TabGroup, "Title", "Time");
             obj.TimeDomainPanel = TimePanel(TimeTab);
 
+            % Frequency Domain
             FrequencyTab = uitab(obj.TabGroup, "Title", "Freuqency");
             obj.FreqDomainPanel = FrequencyPanel(FrequencyTab);
 
+            % Controls
             ControlsTab = uitab(obj.TabGroup, "Title", "Controls");
+        end
+
+        function RunSimCallback(obj)
+            if ~isempty(obj.fwdRunSimCallback)
+                obj.fwdRunSimCallback();
+            end
         end
     end
 end
