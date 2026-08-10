a9f58fc00388940cc9505ec18684072670b684a8
devanshroyal-7 <jdevansh2000@gmail.com>
2026-08-10 19:46:29 +0000

    Implement main.m

--- src/App/main.m
+++ src/App/main.m
@@ -1,9 +1,24 @@
 function main()
     delete(timerfindall);             % close any existing instances
 
-    model       = AppModel();
-    view        = AppView();
-    controller  = AppController();
+    fig = uifigure("Name", "Mechanical System Experimenation App", "Position", [500, 500, 1900, 900]);
 
-    assignin('base', 'mse_app_controllers', controller);
+    model       = AppModel();
+    view        = AppView(fig);
+    controller  = AppController(model, view);
+
+    fig.CloseRequestFcn = @(~, ~) cleanupApp(fig, model, view, controller);
+
+    uiwait(fig);
+    
+    delete(controller)
+    delete(view)
+    if isvalid(fig),    delete(fig);        end
+end
+
+function cleanupApp(fig, model, view, controller)
+    if isvalid(controller), delete(controller); end
+    if isvalid(model),      delete(model);      end
+    if isvalid(view),       delete(view);       end
+    if isvalid(fig),        delete(fig);        end
 end
